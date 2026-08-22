38cfdae13e27c3c90b21db3895ec4bdcb2470a3f
iamman307 <49023364+iamman307@users.noreply.github.com>
2026-08-22 22:13:41 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='stock-ledger-v3-auto';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
